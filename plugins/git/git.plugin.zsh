5fc90c5fae4f628c5925d8d920efbd45ba41cbaf
John Swanson <john.d.swanson@gmail.com>
2013-02-18 20:01:49 -0800

    Removal of git aliases I don't use or need.

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,12 +1,12 @@
 # Aliases
 alias g='git'
 compdef g=git
-alias gst='git status'
-compdef _git gst=git-status
-alias gl='git pull'
-compdef _git gl=git-pull
-alias gup='git pull --rebase'
-compdef _git gup=git-fetch
+alias gl='git lg'
+compdef _git gl=git-log
+alias gs='git status'
+compdef _git gs=git-status
+alias gf='git fetch'
+compdef _git gf=git-fetch
 alias gp='git push'
 compdef _git gp=git-push
 alias gd='git diff'
@@ -14,8 +14,6 @@ gdv() { git diff -w "$@" | view - }
 compdef _git gdv=git-diff
 alias gc='git commit -v'
 compdef _git gc=git-commit
-alias gca='git commit -v -a'
-compdef _git gca=git-commit
 alias gco='git checkout'
 compdef _git gco=git-checkout
 alias gcm='git checkout master'
@@ -23,40 +21,14 @@ alias gr='git remote'
 compdef _git gr=git-remote
 alias grv='git remote -v'
 compdef _git grv=git-remote
-alias grmv='git remote rename'
-compdef _git grmv=git-remote
-alias grrm='git remote remove'
-compdef _git grrm=git-remote
-alias grset='git remote set-url'
-compdef _git grset=git-remote
-alias grup='git remote update'
-compdef _git grset=git-remote
 alias gb='git branch'
 compdef _git gb=git-branch
-alias gba='git branch -a'
-compdef _git gba=git-branch
-alias gcount='git shortlog -sn'
-compdef gcount=git
-alias gcl='git config --list'
 alias gcp='git cherry-pick'
 compdef _git gcp=git-cherry-pick
-alias glg='git log --stat --max-count=5'
-compdef _git glg=git-log
-alias glgg='git log --graph --max-count=5'
-compdef _git glgg=git-log
-alias glgga='git log --graph --decorate --all'
-compdef _git glgga=git-log
-alias gss='git status -s'
-compdef _git gss=git-status
 alias ga='git add'
 compdef _git ga=git-add
 alias gm='git merge'
 compdef _git gm=git-merge
-alias grh='git reset HEAD'
-alias grhh='git reset HEAD --hard'
-alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
-alias gf='git ls-files | grep'
-alias gpoat='git push origin --all && git push origin --tags'
 
 # Will cd into the top of the current repository
 # or submodule.
